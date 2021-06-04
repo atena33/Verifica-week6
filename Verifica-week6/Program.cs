bf75dcbca58f252f5708236fb99e1706a1fd41e2
Anna <annamura833@gmail.com>
2021-06-04 08:38:30 +0200

    Aggiungere i file di progetto.

---
+++ Verifica-week6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Verifica_week6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
